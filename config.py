af5f490635651f96d100d0a30f7710e3fc881f12
jerryxu178 <lx46@cornell.edu>
2019-10-05 17:50:10 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+
+#This file contains the login information and credentials needed to run the bot.
+
+username = "aita5306"
+password = "aita5306"
+client_id = "KMF22GoXdFJfYw"
+client_secret = "SzxlVJ2Ec6nZyXe2AR3mMWolqLk"
+
+sub_reddit = "AmItheAsshole"
---
+++ reddit_bot.py
@@ -0,0 +1,52 @@
+import praw
+import config
+from praw.models import MoreComments
+from operator import itemgetter
+import csv
+
+def scraper_login():
+# log into the reddit account with credentials from config.py
+    r = praw.Reddit(username = config.username,
+        password = config.password,
+        client_id = config.client_id,
+        client_secret = config.client_secret,
+        user_agent = "aita scraper v1.0")
+    return r
+
+def run_scraper(r):
+# run the bot on subreddit designated in config.py
+    post = r.submission(id = 'cihc3z')
+    submission_info = []
+    for top_level_comment in post.comments:
+        if isinstance(top_level_comment, MoreComments): #ignore MoreComments
+            continue
+        #print "start"
+        #print "score: " + str(top_level_comment.score)
+        #print "hours elapsed: " + str((top_level_comment.created_utc-s1.created_utc)/3600)
+        #print "number of replies: " + str(len(top_level_comment.replies))
+        #print "number of words: " + str(len(top_level_comment.body.split(" ")))
+        #print "end
+        
+        submission_info.append([top_level_comment.score
+        , str((top_level_comment.created_utc-post.created_utc)/3600)
+        , str(len(top_level_comment.replies))
+        , str(len(top_level_comment.body.split(" ")))
+        ])
+    submission_info = sorted(submission_info, key=itemgetter(0))[::-1]
+    counter = 1
+    for elt in submission_info:
+        elt.append("R" + str(counter))
+        counter += 1
+    submission_info = submission_info[:10]
+        
+    with open('data1.csv',mode='w') as data:
+        data_writer = csv.writer(data, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
+        
+        for elt in submission_info:
+            data_writer.writerow(elt)  
+        
+        
+
+
+r = scraper_login()
+run_scraper(r)
